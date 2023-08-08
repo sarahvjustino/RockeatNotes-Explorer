16e7507a698b4809046d1452bba318b4a0ba3606
Sarah <sarahvictoria230@gmail.com>
2023-08-08 10:45:05 -0300

    feat: created ButtonText component

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+    font-size: 1rem;
+`
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,11 +4,13 @@ import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir nota" />
       <Section title="Links úteis">
         <Links>
           <li>
