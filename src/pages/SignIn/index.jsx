d4bc50b06276a11560d1eb14c835e22b75865ecb
Sarah <sarahvictoria230@gmail.com>
2023-08-18 22:58:55 -0300

    feat: created and styled Form component from SignIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,26 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form } from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input type="text" placeholder="E-mail" icon={FiMail} />
+        <Input type="password" placeholder="Senha" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -6,3 +6,35 @@ export const Container = styled.div`
     display: flex;
     align-items: stretch;
 `
+
+export const Form = styled.form`
+    padding: 0 8.5rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+
+    text-align: center;
+
+    > h1{
+        font-size: 3rem;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+
+    > h2{
+        font-size: 1.5rem;
+        margin-top: 5.25rem;
+        margin-bottom: 1.5rem;
+    }
+
+    > p {
+        font-size: 0.88rem;
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > a {
+        margin-top: 7.74rem;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+`
