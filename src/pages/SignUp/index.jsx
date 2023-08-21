03b7f7aed6104ad1379363cd43d75f157115eb67
Sarah <sarahvictoria230@gmail.com>
2023-08-21 20:29:02 -0300

    feat: created SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,28 +1,29 @@
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import { Container, Form, Background } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
+
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
+        <Input type="text" placeholder="Nome" icon={FiUser} />
         <Input type="text" placeholder="E-mail" icon={FiMail} />
         <Input type="password" placeholder="Senha" icon={FiLock} />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <a href="#">Criar conta</a>
+        <a href="#">Voltar para o login</a>
       </Form>
-
-      <Background />
     </Container>
   );
 }
